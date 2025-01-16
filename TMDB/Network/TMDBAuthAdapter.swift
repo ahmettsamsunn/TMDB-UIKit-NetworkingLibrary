0f78f20846a4fbf8d144be4544adad797761e1a3
ahmettsamsunn <113214856+ahmettsamsunn@users.noreply.github.com>
2025-01-16 08:13:31 +0300

    .

--- TMDB/Network/TMDBAuthAdapter.swift
+++ TMDB/Network/TMDBAuthAdapter.swift
@@ -4,7 +4,7 @@ import NetworkingLibrary
 struct TMDBAuthAdapter: RequestAdaptation {
     private let apiKey: String
     
-    init(apiKey: String = "f2b4502f48921ca4253b0a570caa027d") {
+    init(apiKey: String = "YOUR_API_KEY_HERE") {
         self.apiKey = apiKey
     }
     
